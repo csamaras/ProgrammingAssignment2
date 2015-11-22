ee14e7909b5dbb2c6d3c5c366b80118073683561
Christos Samaras <christos.samaras@gmail.com>
2015-11-22 11:11:36 +0200

    Add implementation of cacheSolve() function

--- cachematrix.R
+++ cachematrix.R
@@ -26,3 +26,27 @@ makeCacheMatrix <- function(initial_matrix = matrix()) {
     # return a list with setter and getter functions for both the initial matrix and the inverse matrix
     return(list(set = set, get = get, setInverseMatrix = setInverseMatrix, getInverseMatrix = getInverseMatrix))
 }
+
+cacheSolve <- function(initial_matrix, ...) {
+    # get the inverse matrix
+    inverse_matrix <- initial_matrix$getInverseMatrix()
+    
+    # check whether the inverse matrix already exists in cache, and if yes, then return it
+    if(!is.null(inverse_matrix)) {
+        message("getting cached inverse matrix")
+        return(inverse_matrix)
+    }
+    
+    # get the initial matrix (i.e., supplied matrix)
+    init_mtx <- initial_matrix$get()
+    
+    # compute the inverse matrix
+    # (note: it is assumed that the matrix supplied is always invertible)
+    inverse_matrix <- solve(init_mtx)
+    
+    # set the inverse matrix of the initial matrix
+    initial_matrix$setInverseMatrix(inverse_matrix)
+    
+    # return the inverse matrix
+    return(inverse_matrix)
+}
